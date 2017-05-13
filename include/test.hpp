1d782d6f38615760b2c2be0ace8762560cc91356
Zarakichi <pierrickleplenier@gmail.com>
2017-05-13 21:17:17 +0200

    Initial commit

---
+++ include/test.hpp
@@ -0,0 +1,7 @@
+#include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+
+int fonctionCool()
+{
+    return 5;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test.hpp"
+
+int main(int argc, char** argv)
+{
+	int a;
+	std::cin >> a;
+    return 0;
+}
